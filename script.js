ad0d0d41eb7c9249984fafcad286d23db15fc41d
lion-oldriver <w879443a72cj2i@gmail.com>
2021-12-23 17:47:04 +0900

    arrow function

---
+++ script.js
@@ -0,0 +1,25 @@
+let dom = document.querySelector('#root')
+
+let element = React.createElement(
+  'div', {}, [
+    React.createElement(
+      'h2', {}, 'Hello'
+    ),
+    React.createElement(
+      'h4', {className: 'alert alert-primary'}, 'React sample page'
+    ),
+    React.createElement(
+      'ul', {className: 'list-group'}, [
+        React.createElement(
+         'li', {className: 'list-group-item'}, "First Item" 
+        ),
+        React.createElement(
+          'li', {className: 'list-group-item'}, "Second Item" 
+        ),
+        React.createElement(
+          'li', {className: 'list-group-item'}, "Third Item" 
+        ),
+      ]
+    ),
+  ])
+ReactDOM.render(element, dom)
